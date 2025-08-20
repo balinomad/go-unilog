56564eea6392d9701fc22c1dcc0b99bbf4c0bfc1
Miklos Karpati <miklos888@gmail.com>
2025-08-20 12:38:30 +0800

    Logger interface refactored, fallback logger adjusted

--- fallbacklogger.go
+++ fallbacklogger.go
@@ -6,19 +6,24 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 	"sync/atomic"
 )
 
 // fallbackLogger provides a minimal, panic-safe Logger implementation.
 // It is used as the default logger when no other logger has been configured via SetDefault.
 // Safe for concurrent use by multiple goroutines.
+//
+// Not meant for production use.
 type fallbackLogger struct {
 	w   *AtomicWriter
 	l   *log.Logger
 	lvl atomic.Int32
 }
 
-func (l *fallbackLogger) Sync() error { return nil }
+// Ensure fallbackLogger implements the unilog.Logger and unilog.Configurator interfaces.
+var _ Logger = (*fallbackLogger)(nil)
+var _ Configurator = (*fallbackLogger)(nil)
 
 // newFallbackLogger creates a new fallbackLogger with the given output writer.
 func newFallbackLogger(w io.Writer, level LogLevel) (*fallbackLogger, error) {
@@ -42,32 +47,29 @@ func (l *fallbackLogger) Log(_ context.Context, level LogLevel, msg string, keyV
 		return
 	}
 
-	var kvs string
-	if len(keyValues) > 0 {
-		kvs = fmt.Sprintf(" %v", keyValues)
+	sb := strings.Builder{}
+	sb.WriteString(level.String())
+	sb.WriteString(": ")
+	sb.WriteString(msg)
+	for i := 0; i < len(keyValues)-1; i += 2 {
+		sb.WriteString(" ")
+		sb.WriteString(fmt.Sprint(keyValues[i]))
+		sb.WriteString("=")
+		sb.WriteString(fmt.Sprint(keyValues[i+1]))
 	}
 
-	l.l.Printf("%s: %s%s", level.String(), msg, kvs)
+	l.l.Println(sb.String())
 
 	if level == LevelFatal {
 		os.Exit(1)
 	}
 }
 
-// With is not implemented for the fallback logger.
+// With is a no-op for for the fallback logger. It returns itself unchanged.
 func (l *fallbackLogger) With(keyValues ...any) Logger { return l }
 
-// WithGroup is not implemented for the fallback logger.
+// WithGroup is a no-op for for the fallback logger. It returns itself unchanged.
 func (l *fallbackLogger) WithGroup(name string) Logger { return l }
-
-// CallerSkip is not implemented for the fallback logger.
-func (l *fallbackLogger) CallerSkip() int { return 0 }
-
-// WithCallerSkip is not implemented for the fallback logger.
-func (l *fallbackLogger) WithCallerSkip(skip int) (Logger, error) { return nil, nil }
-
-// WithCallerSkipDelta is not implemented for the fallback logger.
-func (l *fallbackLogger) WithCallerSkipDelta(delta int) (Logger, error) { return nil, nil }
 
 func (l *fallbackLogger) SetLevel(level LogLevel) error {
 	if err := ValidateLogLevel(level); err != nil {
@@ -87,74 +89,32 @@ func (l *fallbackLogger) Enabled(level LogLevel) bool {
 	return level >= LogLevel(l.lvl.Load())
 }
 
-// Debug logs a message at the debug level. It is a convenience wrapper around Log,
-// using the current background context and the LevelDebug level.
-func (l *fallbackLogger) Debug(msg string, keyValues ...any) {
-	l.Log(context.Background(), LevelDebug, msg, keyValues...)
-}
-
-// Info logs a message at the info level. It is a convenience wrapper around Log,
-// using the current background context and the LevelInfo level.
-func (l *fallbackLogger) Info(msg string, keyValues ...any) {
-	l.Log(context.Background(), LevelInfo, msg, keyValues...)
-}
-
-// Warn logs a message at the warn level. It is a convenience wrapper around Log,
-// using the current background context and the LevelWarn level.
-func (l *fallbackLogger) Warn(msg string, keyValues ...any) {
-	l.Log(context.Background(), LevelWarn, msg, keyValues...)
-}
-
-// Error logs a message at the error level. It is a convenience wrapper around Log,
-// using the current background context and the LevelError level.
-func (l *fallbackLogger) Error(msg string, keyValues ...any) {
-	l.Log(context.Background(), LevelError, msg, keyValues...)
-}
-
-// Critical logs a message at the critical level. It is a convenience wrapper around Log,
-// using the current background context and the LevelCritical level.
-func (l *fallbackLogger) Critical(msg string, keyValues ...any) {
-	l.Log(context.Background(), LevelCritical, msg, keyValues...)
-}
-
-// Fatal logs a message at the fatal level. It is a convenience wrapper around Log,
-// using the current background context and the LevelFatal level.
-func (l *fallbackLogger) Fatal(msg string, keyValues ...any) {
-	l.Log(context.Background(), LevelFatal, msg, keyValues...)
-}
-
-// DebugCtx logs a message at the debug level. It is a convenience wrapper around Log,
-// using the provided context and the LevelDebug level.
-func (l *fallbackLogger) DebugCtx(ctx context.Context, msg string, keyValues ...any) {
+// Debug logs a message at the debug level.
+func (l *fallbackLogger) Debug(ctx context.Context, msg string, keyValues ...any) {
 	l.Log(ctx, LevelDebug, msg, keyValues...)
 }
 
-// InfoCtx logs a message at the info level. It is a convenience wrapper around Log,
-// using the provided context and the LevelInfo level.
-func (l *fallbackLogger) InfoCtx(ctx context.Context, msg string, keyValues ...any) {
+// Info logs a message at the info level.
+func (l *fallbackLogger) Info(ctx context.Context, msg string, keyValues ...any) {
 	l.Log(ctx, LevelInfo, msg, keyValues...)
 }
 
-// WarnCtx logs a message at the warn level. It is a convenience wrapper around Log,
-// using the provided context and the LevelWarn level.
-func (l *fallbackLogger) WarnCtx(ctx context.Context, msg string, keyValues ...any) {
+// Warn logs a message at the warn level.
+func (l *fallbackLogger) Warn(ctx context.Context, msg string, keyValues ...any) {
 	l.Log(ctx, LevelWarn, msg, keyValues...)
 }
 
-// ErrorCtx logs a message at the error level. It is a convenience wrapper around Log,
-// using the provided context and the LevelError level.
-func (l *fallbackLogger) ErrorCtx(ctx context.Context, msg string, keyValues ...any) {
+// Error logs a message at the error level.
+func (l *fallbackLogger) Error(ctx context.Context, msg string, keyValues ...any) {
 	l.Log(ctx, LevelError, msg, keyValues...)
 }
 
-// CriticalCtx logs a message at the critical level. It is a convenience wrapper around Log,
-// using the provided context and the LevelCritical level.
-func (l *fallbackLogger) CriticalCtx(ctx context.Context, msg string, keyValues ...any) {
+// Critical logs a message at the critical level.
+func (l *fallbackLogger) Critical(ctx context.Context, msg string, keyValues ...any) {
 	l.Log(ctx, LevelCritical, msg, keyValues...)
 }
 
-// FatalCtx logs a message at the fatal level. It is a convenience wrapper around Log,
-// using the provided context and the LevelFatal level.
-func (l *fallbackLogger) FatalCtx(ctx context.Context, msg string, keyValues ...any) {
+// Fatal logs a message at the fatal level.
+func (l *fallbackLogger) Fatal(ctx context.Context, msg string, keyValues ...any) {
 	l.Log(ctx, LevelFatal, msg, keyValues...)
 }
--- logger.go
+++ logger.go
@@ -9,55 +9,58 @@ import (
 // abstraction over various logging libraries, enabling structured and
 // level-based logging.
 type Logger interface {
-	// Sync flushes any buffered log entries.
-	Sync() error
-
 	// Log is the core logging method. All other level-specific methods (Debug, Info, etc.)
 	// are convenience wrappers around this method.
 	Log(ctx context.Context, level LogLevel, msg string, keyValues ...any)
+
+	// Enabled returns true if the logger is configured for the given level.
+	Enabled(level LogLevel) bool
 
 	// With adds structured context to the logger. It returns a new logger
 	// instance that will include the given key-value pairs in all subsequent
 	// log messages. The underlying implementation should handle this immutably.
 	With(keyValues ...any) Logger
-
 	// WithGroup returns a Logger that starts a group, if name is non-empty.
 	// The keys of all attributes added to the Logger will be qualified by the given name.
 	WithGroup(name string) Logger
 
+	// Convenience methods for logging at specific levels.
+	Debug(ctx context.Context, msg string, keyValues ...any)
+	Info(ctx context.Context, msg string, keyValues ...any)
+	Warn(ctx context.Context, msg string, keyValues ...any)
+	Error(ctx context.Context, msg string, keyValues ...any)
+	Critical(ctx context.Context, msg string, keyValues ...any)
+	Fatal(ctx context.Context, msg string, keyValues ...any)
+}
+
+// Configurator is an interface for loggers that support dynamic configuration.
+type Configurator interface {
 	// SetLevel dynamically changes the minimum level of logs.
 	SetLevel(level LogLevel) error
-
 	// SetOutput changes the destination for log output.
 	SetOutput(w io.Writer) error
+}
 
+// CallerSkipper is an interface for loggers that support dynamic caller skipping.
+type CallerSkipper interface {
 	// CallerSkip returns the current number of stack frames being skipped.
 	CallerSkip() int
-
 	// WithCallerSkip returns a new Logger instance with the caller skip value updated.
 	// The original logger is not modified.
 	WithCallerSkip(skip int) (Logger, error)
-
 	// WithCallerSkipDelta returns a new Logger instance with the caller skip value altered by the given delta.
 	// The original logger is not modified.
 	WithCallerSkipDelta(delta int) (Logger, error)
+}
 
-	// Enabled returns true if the logger is configured for the given level.
-	Enabled(level LogLevel) bool
+// Cloner is an interface for loggers that support cloning.
+type Cloner interface {
+	// Clone returns a deep copy of the logger.
+	Clone() Logger
+}
 
-	// Convenience methods for logging at specific levels.
-	Debug(msg string, keyValues ...any)
-	Info(msg string, keyValues ...any)
-	Warn(msg string, keyValues ...any)
-	Error(msg string, keyValues ...any)
-	Critical(msg string, keyValues ...any)
-	Fatal(msg string, keyValues ...any)
-
-	// Convenience methods for logging with a context.
-	DebugCtx(ctx context.Context, msg string, keyValues ...any)
-	InfoCtx(ctx context.Context, msg string, keyValues ...any)
-	WarnCtx(ctx context.Context, msg string, keyValues ...any)
-	ErrorCtx(ctx context.Context, msg string, keyValues ...any)
-	CriticalCtx(ctx context.Context, msg string, keyValues ...any)
-	FatalCtx(ctx context.Context, msg string, keyValues ...any)
+// Syncer is an interface for loggers that support flushing.
+type Syncer interface {
+	// Sync flushes any buffered log entries.
+	Sync() error
 }
